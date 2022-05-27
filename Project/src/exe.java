9b1a6d32ab4786c4f4e0a3f72689d53a9fb50e64
sk <78210333+sk-zo@users.noreply.github.com>
2022-05-27 14:25:07 +0900

    Delete Project directory

--- Project/src/exe.java
+++
@@ -1,7 +0,0 @@
-public class exe {
-	public static void main(String[] args) {
-		OracleDB db = new OracleDB();
-		
-		Loops.main_loop(db);
-	}
-}
--- Project/src/models/Common.java
+++
@@ -1,8 +0,0 @@
-package models;
-
-public class Common {
-	private String name;
-	private int hp;
-	private int lv;
-	private int vp;
-}
